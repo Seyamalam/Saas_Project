917b6075db7426c8e1294865ddd6b6904a3cc06e
Seyamalam <seyamalam41@gmail.com>
2024-06-19 01:19:42 +0600

    Fixed views.py formatting

--- src/seyamhome/views.py
+++ src/seyamhome/views.py
@@ -11,13 +11,13 @@ def home_page_view(request, *args, **kwargs):
     qs = PageVisit.objects.filter(path=request.path)
     page_qs = PageVisit.objects.filter(path=request.path)
     my_title = "My Page"
+    html_template = "home.html"
     my_context = {
         "page_title": my_title,
         "page_visit_count": page_qs.count(),
         "total_visit_count": qs.count(),
         "percent": page_qs.count() / qs.count() * 100
     }
-    html_template = "home.html"
     PageVisit.objects.create(path=request.path)
     return render(request, html_template, my_context)
 
