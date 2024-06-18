1ddf677291a8c4ed070fee015b7d7896940a3186
Seyamalam <seyamalam41@gmail.com>
2024-06-19 01:12:20 +0600

    helloworldmsg

--- src/seyamhome/urls.py
+++ src/seyamhome/urls.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 
-from.views import homea_page_view
+from.views import home_page_view
 
 urlpatterns = [
-    path("", homea_page_view),  #index-page
-    path("hello-world/", homea_page_view),
-    path("hello-world.html", homea_page_view),
+    path("", home_page_view),  #index-page
+    path("hello-world/", home_page_view),
+    path("hello-world.html", home_page_view),
     path("admin/", admin.site.urls),
 ]
